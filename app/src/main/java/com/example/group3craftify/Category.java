906072f1f895b4f63d76765a4df909b05f32a47f
George <cortes.george93@icloud.com>
2024-04-13 20:18:24 -0700

    testing new code

--- app/src/main/java/com/example/group3craftify/Category.java
+++ app/src/main/java/com/example/group3craftify/Category.java
@@ -1,2 +1,47 @@
-package com.example.group3craftify;public class Category {
+package com.example.group3craftify;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Category extends RecyclerView.Adapter<Category.ViewHolder>{
+    private ArrayList<String>categoryList = new ArrayList<>();
+
+    public Category() {
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoryList.size();
+    }
+
+    public ArrayList<String> getCategoryList() {
+        return categoryList;
+    }
+
+    public void setCategoryList(ArrayList<String> categoryList) {
+        this.categoryList = categoryList;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
--- app/src/main/java/com/example/group3craftify/MainActivity.java
+++ app/src/main/java/com/example/group3craftify/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.group3craftify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button butto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+//        Test code below here
+        butto = findViewById(R.id.button);
+        butto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,categoriesActivity.class);
+            startActivity(intent);
+
+            }
         });
     }
 }
