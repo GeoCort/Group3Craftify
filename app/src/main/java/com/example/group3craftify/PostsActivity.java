264f8305fb54bf8f3f7b59dd864786d02f81a4ea
George <cortes.george93@icloud.com>
2024-04-20 03:01:16 -0700

    checked that correct post info is passed into post activity

--- app/src/main/java/com/example/group3craftify/PostsActivity.java
+++ app/src/main/java/com/example/group3craftify/PostsActivity.java
@@ -9,14 +9,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class PostsActivity extends AppCompatActivity {
 String craftRefKey;
+FirebaseDatabase db;
+DatabaseReference PostRef;
+FirebaseAuth user;
+User currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_posts);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.postMain), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
@@ -24,7 +32,11 @@ String craftRefKey;
         Intent fromCrafts = getIntent();
         if(fromCrafts != null){
             String craftRefKey = fromCrafts.getStringExtra("craft");
+            System.out.println(craftRefKey); // works
+
+
         }
 
     }
+
 }
