457ea37109262815c835adb5e46f0c7e93dfc90f
George <george.ermakov95@gmail.com>
2024-04-21 11:04:50 -0700

    splash screen

---
+++ app/src/main/java/com/example/group3craftify/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.group3craftify;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DISPLAY_LENGTH = 3000; // Duration in milliseconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
