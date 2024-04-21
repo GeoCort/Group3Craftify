8efb80ca6cea008c5d275f1df10ae3cd4b5bc098
George <george.ermakov95@gmail.com>
2024-04-21 11:48:06 -0700

    updated animations for splash screen working

--- app/src/main/java/com/example/group3craftify/SplashScreenActivity.java
+++ app/src/main/java/com/example/group3craftify/SplashScreenActivity.java
@@ -3,21 +3,44 @@ package com.example.group3craftify;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-    private static final int SPLASH_DISPLAY_LENGTH = 3000; // Duration in milliseconds
+    private static final int SPLASH_DISPLAY_LENGTH = 3000; // Total duration in milliseconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        ImageView logo = findViewById(R.id.imageViewLogo);
+        Animation scaleUp = AnimationUtils.loadAnimation(this, R.anim.scale_up);
+        Animation scaleDown = AnimationUtils.loadAnimation(this, R.anim.scale_down);
+
+        scaleUp.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                logo.startAnimation(scaleDown);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        logo.startAnimation(scaleUp);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                // This method will be executed once the timer is over
                 Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
                 SplashScreenActivity.this.startActivity(mainIntent);
                 SplashScreenActivity.this.finish();
