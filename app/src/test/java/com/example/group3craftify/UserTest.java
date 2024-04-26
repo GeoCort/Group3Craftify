773a1c024ecaf9c3e0f2e2e11d1181763e28251a
George Cortes <93002810+GeoCort@users.noreply.github.com>
2024-04-26 10:54:46 -0700

    User Test class added

--- app/src/test/java/com/example/group3craftify/UserTest.java
+++ app/src/test/java/com/example/group3craftify/UserTest.java
@@ -7,27 +7,38 @@ import org.junit.Test;
 public class UserTest extends TestCase {
     @Test
     public void testGetUsername() {
-        User user = new User();
-        user.setUserID("JohnDoe");
+        User user = new User("2021","2122@mail.com","2i222");
+        user.setUsername("JohnDoe");
         assertEquals("JohnDoe", user.getUsername());
 
     }
     @Test
     public void testSetUsername() {
         User user = new User();
-        user.setUserID("JohnDoe");
+        user.setUsername("JohnDoe");
         assertEquals("JohnDoe", user.getUsername());
     }
 
     public void testGetEmail() {
+        User user = new User("2021","2122@mail.com","2i222");
+        assertEquals("2122@mail.com", user.getEmail());
     }
 
     public void testSetEmail() {
+        User user = new User();
+        user.setEmail("mynewemail@gmail.com");
+        assertEquals("mynewemail@gmail.com",user.getEmail());
     }
 
     public void testGetUserID() {
+        User user = new User();
+        user.setUserID("49951iop00023");
+        assertEquals("49951iop00023", user.getUserID());
     }
 
     public void testSetUserID() {
+        User user = new User("2021","2122@mail.com","2i222");
+        user.setUserID("49951iop00023");
+        assertEquals("49951iop00023", user.getUserID());
     }
 }
