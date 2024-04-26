92ab7acd54d6a48edb08b73596f7623690bac6f4
George <cortes.george93@icloud.com>
2024-04-25 21:33:00 -0700

    style changes

--- app/src/test/java/com/example/group3craftify/UserTest.java
+++ app/src/test/java/com/example/group3craftify/UserTest.java
@@ -1,4 +1,33 @@
+package com.example.group3craftify;
+
 import junit.framework.TestCase;
+
+import org.junit.Test;
+
 public class UserTest extends TestCase {
-  
+    @Test
+    public void testGetUsername() {
+        User user = new User();
+        user.setUserID("JohnDoe");
+        assertEquals("JohnDoe", user.getUsername());
+
+    }
+    @Test
+    public void testSetUsername() {
+        User user = new User();
+        user.setUserID("JohnDoe");
+        assertEquals("JohnDoe", user.getUsername());
+    }
+
+    public void testGetEmail() {
+    }
+
+    public void testSetEmail() {
+    }
+
+    public void testGetUserID() {
+    }
+
+    public void testSetUserID() {
+    }
 }
