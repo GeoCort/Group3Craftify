8a37886c26174ea24908957bfcdc9aef6eaf69b8
George Cortes <93002810+GeoCort@users.noreply.github.com>
2024-04-26 17:08:04 -0700

    user class javadoc

--- app/src/main/java/com/example/group3craftify/User.java
+++ app/src/main/java/com/example/group3craftify/User.java
@@ -1,6 +1,8 @@
 package com.example.group3craftify;
 
-// Define a class named User
+/**
+ *
+ */
 public class User {
 
     // Declare instance variables for username, email, and userID
@@ -8,16 +10,27 @@ public class User {
     public String email;
     String userID; // This variable is package-private, accessible only within the same package
 
-    // Default constructor
+    /**
+     * Empty constructor for firebase
+     */
     public User() {}
 
-    // Constructor with parameters for username and email
+    /**
+     * Creates a user object before being added to a DB
+     * @param username the username given by the user
+     * @param email the email given by the user
+     */
     public User(String username, String email) {
         this.username = username;
         this.email = email;
     }
 
-    // Constructor with parameters for username, email, and userID
+    /**
+     * Generates a user that has already had an ID created
+     * @param username username
+     * @param email email
+     * @param userID userID ideally from DB or generated previously
+     */
     public User(String username, String email, String userID) {
         this.username = username;
         this.email = email;
@@ -25,31 +38,51 @@ public class User {
     }
 
     // Getter method for username
+
+    /**
+     * Gets the username of the user
+     * @return String
+     */
     public String getUsername() {
         return username;
     }
 
-    // Setter method for username
+    /**
+     * Sets the user's username to username
+     * @param username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
-    // Getter method for email
+    /**
+     * returns the email of the current user
+     * @return String
+     */
     public String getEmail() {
         return email;
     }
 
-    // Setter method for email
+    /**
+     * sets the username's email to email
+     * @param email new email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
-    // Getter method for userID
+    /**
+     * gets the current user's ID
+     * @return userID
+     */
     public String getUserID() {
         return userID;
     }
 
-    // Setter method for userID
+    /**
+     * sets the userID to userID
+     * @param userID the new userID for user
+     */
     public void setUserID(String userID) {
         this.userID = userID;
     }
